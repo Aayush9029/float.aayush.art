ebbe7b2c04244feef46ba3f125e1e353f04b2ae5
Aayush9029 <developer@aayush.art>
2023-12-12 08:35:54 -0500

    updated few files

---
+++ components/pages/legal/tos/index.js
@@ -0,0 +1,100 @@
+import { Section } from "@/components/common/layout";
+
+export default function TOSPage() {
+  return (
+    <Section>
+      <h2>End-User License Agreement (EULA) for Float</h2>
+
+      <p>
+        This End-User License Agreement ("EULA") is a binding legal contract
+        between you (either an individual or a single entity) and Float, the
+        developer of the Float software application.
+      </p>
+
+      <p>
+        This EULA governs your acquisition and use of the Float software
+        ("Software"), whether obtained directly from Float or through a Float
+        authorized reseller or distributor ("Reseller").
+      </p>
+
+      <p>
+        Before installing and using the Float software, please read this EULA
+        carefully. It grants a license to use the Float software and includes
+        warranty information and liability disclaimers. Your agreement to these
+        terms is signified by clicking "accept" or by installing and using the
+        Float software.
+      </p>
+
+      <p>
+        If you are accepting this EULA on behalf of a company or other legal
+        entity, you confirm that you have the authority to bind such entity to
+        these terms. If you lack such authority, or if you do not agree with
+        these terms, you must not install or use the Software.
+      </p>
+
+      <p>
+        This EULA applies exclusively to the Float Software and does not cover
+        any other software, even if referenced or described herein. The terms
+        also apply to Float updates, Internet-based services, and support
+        services for the Software, unless other terms are provided with those
+        items.
+      </p>
+
+      <h3>License Grant</h3>
+
+      <p>
+        Float grants you a personal, non-transferable, non-exclusive license to
+        use the Float software on your devices in accordance with this EULA.
+      </p>
+
+      <p>
+        You may install the Float software on devices under your control,
+        ensuring they meet the minimum requirements of the software.
+      </p>
+
+      <p>Prohibited Actions:</p>
+      <ul>
+        <li>
+          Modifying, translating, or creating derivative works from the
+          Software.
+        </li>
+        <li>
+          Copying, redistributing, or commercializing the Software without
+          authorization.
+        </li>
+        <li>
+          Allowing third parties to use the Software on your behalf or for their
+          benefit.
+        </li>
+        <li>
+          Using the Software in violation of applicable laws or this EULA.
+        </li>
+      </ul>
+
+      <h3>Intellectual Property and Ownership</h3>
+
+      <p>
+        Float retains all ownership rights to the Software, including all
+        intellectual property rights therein, regardless of any modifications.
+        Float may grant third-party licenses for the Software at its discretion.
+      </p>
+
+      <h3>Termination</h3>
+
+      <p>
+        This EULA is effective from your first use of the Software and continues
+        until terminated. You may terminate it anytime with written notice to
+        Float. It also terminates immediately upon your failure to comply with
+        any term. Upon termination, you must cease all use of the Software.
+        Provisions that naturally survive termination will remain in effect.
+      </p>
+
+      <h3>Governing Law</h3>
+
+      <p>
+        This EULA and any disputes related to it are governed by the laws of
+        Ontario, Canada.
+      </p>
+    </Section>
+  );
+}
--- pages/legal/privacy.js
+++ pages/legal/privacy.js
@@ -1,1 +1,80 @@
-export { default } from '@/components/pages/legal/privacy';
+import { Section } from "@/components/common/layout";
+
+export default function PrivacyPage() {
+  return (
+    <Section>
+      <Section contained gutterY>
+        <h1>Our Privacy Policy</h1>
+      </Section>
+
+      <Section>
+        <h2>Introduction and Overview</h2>
+        <p>
+          This Privacy Policy outlines our commitment to protecting the privacy
+          and security of the users of our application (hereinafter referred to
+          as "Float"). We understand the importance of privacy and have designed
+          Float with your right to privacy as a fundamental consideration. This
+          document explains how we handle and treat your data and your rights in
+          relation to that data.
+        </p>
+      </Section>
+
+      <Section>
+        <h2>Your Data in Float</h2>
+        <p>
+          As a user of Float, you retain complete control over your data. We
+          have no access to any of your data stored or processed by Float on
+          your device. Our philosophy is to ensure that your data remains
+          strictly within your device, and we do not, under any circumstances,
+          collect, store, or transmit personal data through Float.
+        </p>
+      </Section>
+
+      <Section>
+        <h2>Data Security</h2>
+        <p>
+          Ensuring the security of your data within Float is a top priority. We
+          employ advanced security measures to prevent unauthorized access,
+          disclosure, alteration, or destruction of the data stored on your
+          device. Our approach to data security is proactive and vigilant,
+          safeguarding your information against emerging security threats.
+        </p>
+      </Section>
+
+      <Section>
+        <h2>Analytics and Usage Data</h2>
+        <p>
+          Float, in its default configuration, does not explicitly collect,
+          store, or use any analytics or usage information. Our commitment to
+          your privacy extends to abstaining from tracking your usage patterns,
+          preferences, or behavior within Float. However, we may collect
+          optional telemetry data, such as crash logs, to enhance the
+          functionality and stability of Float. This collection is entirely
+          optional and can be enabled or disabled based on your preference.
+        </p>
+      </Section>
+
+      <Section>
+        <h2>Apple’s Analytics Information</h2>
+        <p>
+          Please note that depending on your device’s privacy settings, Apple
+          Inc. may collect and share analytics information, such as crash logs
+          and battery usage data. This is a standard practice for apps
+          distributed through Apple's platforms and is subject to Apple's
+          privacy policies and procedures.
+        </p>
+      </Section>
+
+      <Section>
+        <h2>Changes to Our Privacy Policy</h2>
+        <p>
+          We reserve the right to modify this Privacy Policy at any time. Any
+          changes will be effective immediately upon posting the revised policy.
+          We encourage you to periodically review this page for the latest
+          information on our privacy practices.
+        </p>
+        <br />
+      </Section>
+    </Section>
+  );
+}
